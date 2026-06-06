4e186bf77b15678bb04611d20f97e7ab2ed3fad6
Pavithra2625 <pavii2625@gmail.com>
2026-06-06 15:04:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(section => {
+
+        const top = window.scrollY;
+        const offset = section.offsetTop - 300;
+
+        if(top > offset){
+            section.classList.add("show");
+        }
+
+    });
+
+});
